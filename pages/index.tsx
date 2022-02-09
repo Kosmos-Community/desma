ef6854a3c9ca8ae5661e874b5cf5a78ddbd92835
Jorge Reyes <reyestrevjorge@gmail.com>
2022-02-09 11:41:18 -0600

    Create index.tsx page file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,27 @@
-import { Button } from "@nextui-org/react";
+import { Container, Text } from "@nextui-org/react";
 
 const Index = () => {
-  return <Button>Click Me</Button>;
+  return (
+    <Container
+      css={{
+        width: "100%",
+        padding: "8px",
+        textAlign: "center",
+      }}
+    >
+      <Text
+        small
+        weight="medium"
+        css={{
+          color: "white",
+          textGradient: "45deg, $yellow500 -20%, $red500 100%",
+        }}
+      >
+        Desma: a design system manager
+      </Text>
+      {" 🚀"}
+    </Container>
+  );
 };
 
 export default Index;
