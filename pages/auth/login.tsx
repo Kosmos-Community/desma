9356b67d7bb223d84695f50cb723faa1cbe38283
Jorge Reyes <reyestrevjorge@gmail.com>
2022-02-12 11:43:26 -0600

    Add basic auth routes

---
+++ pages/auth/login.tsx
@@ -0,0 +1,5 @@
+const LoginScreen = () => {
+  return <h1>Login Page</h1>;
+};
+
+export default LoginScreen;
---
+++ pages/auth/register.tsx
@@ -0,0 +1,5 @@
+const RegisterScreen = () => {
+  return <h1>Register Page</h1>;
+};
+
+export default RegisterScreen;
