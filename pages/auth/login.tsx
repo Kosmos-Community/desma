f391ea184c48b36e4d9c7babaf11c3190c7eff58
Jorge Reyes <reyestrevjorge@gmail.com>
2022-02-12 12:26:22 -0600

    Implement states for the login form

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,5 +1,61 @@
+import { Container, Input, Button, Text, Link, Spacer } from '@nextui-org/react';
+import { useState } from 'react';
+
 const LoginScreen = () => {
-  return <h1>Login Page</h1>;
+  const [loginForm, setLoginForm] = useState({ email: '', password: '' });
+
+  const onInputChange = (event) => {
+    const updatedValue = {};
+    updatedValue[event.target.name] = event.target.value;
+    setLoginForm({ ...loginForm, ...updatedValue });
+  };
+
+  const onFormSubmit = () => {
+    if (loginForm.email && loginForm.password) {
+      console.log(loginForm);
+    }
+  };
+
+  return (
+    <Container
+      display="flex"
+      direction="column"
+      justify="center"
+      css={{ maxWidth: '350px', height: '100vh' }}
+    >
+      <Text
+        h1
+        css={{
+          textGradient: '45deg, $blue500 -20%, $pink500 50%',
+        }}
+      >
+        Desma
+      </Text>
+      <Spacer y={2} />
+      <Input
+        labelPlaceholder="Email"
+        name="email"
+        value={loginForm.email}
+        onChange={onInputChange}
+        required
+      />
+      <Spacer y={2} />
+      <Input.Password
+        labelPlaceholder="Password"
+        name="password"
+        value={loginForm.password}
+        onChange={onInputChange}
+        required
+      />
+      <Spacer y={2} />
+      <Button css={{ width: '100%', marginBottom: '.5rem' }} onClick={onFormSubmit}>
+        Login
+      </Button>
+      <Text small>
+        Need an account? <Link href="/auth/register">Register</Link>
+      </Text>
+    </Container>
+  );
 };
 
 export default LoginScreen;
--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,5 +1,34 @@
+import { Container, Input, Button, Text, Link, Spacer } from '@nextui-org/react';
+
 const RegisterScreen = () => {
-  return <h1>Register Page</h1>;
+  return (
+    <Container
+      display="flex"
+      direction="column"
+      justify="center"
+      css={{ maxWidth: '350px', height: '100vh' }}
+    >
+      <Text
+        h1
+        css={{
+          textGradient: '45deg, $blue500 -20%, $pink500 50%',
+        }}
+      >
+        Desma
+      </Text>
+      <Spacer y={2} />
+      <Input labelPlaceholder="Email" />
+      <Spacer y={2} />
+      <Input.Password labelPlaceholder="Password" />
+      <Spacer y={2} />
+      <Input.Password labelPlaceholder="Confirm Password" />
+      <Spacer y={2} />
+      <Button css={{ width: '100%', marginBottom: '.5rem' }}>Register</Button>
+      <Text small>
+        Already have an account? <Link href="/auth/login">Login</Link>
+      </Text>
+    </Container>
+  );
 };
 
 export default RegisterScreen;
