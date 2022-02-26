154f97724787f5a7dbfd6a4f58465377e12c13ad
eduardo.cruz <eduardo.cruz@konfio.mx>
2022-02-25 19:07:39 -0600

    refactor: tabs context

--- pages/designer/index.tsx
+++ pages/designer/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Input, Spacer, Text } from '@nextui-org/react';
 import { Tab } from '../../src/components/atoms/Tab';
 import { TabList } from '../../src/components/atoms/TabList';
@@ -10,11 +10,13 @@ import SpacingSection from '../../src/components/organisms/SpacingSection';
 import { Tabs } from '../../src/context/TabsContext';
 
 const Home = () => {
+  const [tabState, setTabState] = useState(0);
+  
   return (
     <AppLayout>
       <Input size="xl" bordered shadow={false} placeholder="Untitled Design System" />
       <Spacer y={1} />
-      <Tabs>
+      <Tabs value={{ tabState, setTabState }}>
         <TabList>
           <Tab>Colors</Tab>
           <Tab>Fonts</Tab>
--- src/context/TabsContext.tsx
+++ src/context/TabsContext.tsx
@@ -1,21 +1,13 @@
-import { createContext, Dispatch, SetStateAction, useContext, useState } from 'react';
+import { createContext, Dispatch, SetStateAction, useContext } from 'react';
 
 interface ITabs {
   tabState?: number;
   setTabState?: Dispatch<SetStateAction<number>>;
 }
 
-export const TabsContext = createContext<ITabs>({});
+const TabsContext = createContext<ITabs>({});
 
-export const Tabs = ({ children }) => {
-  const [tabState, setTabState] = useState(0);
-
-  return (
-    <TabsContext.Provider value={{ tabState, setTabState }}>
-      {children}
-    </TabsContext.Provider>
-  );
-};
+export const Tabs = TabsContext.Provider;
 
 const useTabsContext = () => {
   const { tabState, setTabState } = useContext(TabsContext);
