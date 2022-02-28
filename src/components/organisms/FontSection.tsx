1a1b4f6d55d686b62718f8c868985aa902368a82
PedroEdu6786 <pedrcg835@gmail.com>
2022-02-27 23:21:17 -0600

    fix: font link rendering and multiple word fonts (Open Sans)

--- src/components/organisms/FontSection.tsx
+++ src/components/organisms/FontSection.tsx
@@ -2,7 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { Button, Grid, Input, Row, Spacer, Text } from '@nextui-org/react';
 import useDesignContext, { EDesignAction } from '../../context/DesignContext';
 import Dropdown from '../atoms/Dropdown';
-import { SCALE_OPTIONS, TABLE_HEADERS } from '../../utils/constants';
+import {
+  GOOGLE_FONTS_URL,
+  SCALE_OPTIONS,
+  TABLE_HEADERS,
+  WEIGHT_QUERY,
+} from '../../utils/constants';
 import Table from '../molecules/Table';
 import { handleFontRatio } from '../../utils/scaleFactor';
 import FontContainer from '../atoms/FontContainer';
@@ -16,16 +21,30 @@ const FontSection = () => {
   const { headingFontName, paragraphFontName } = designData.font;
 
   useEffect(() => {
-    const headingLink = `https://fonts.googleapis.com/css2?family=${headingFontName}:wght@400;700&display=swap`;
-    const paragraphLink = `https://fonts.googleapis.com/css2?family=${paragraphFontName}:wght@400;700&display=swap`;
+    const newHeading = headingFontName.replace(/\s/g, '+');
+    const newParagraph = paragraphFontName.replace(/\s/g, '+');
+    const headingLink = `${GOOGLE_FONTS_URL}${newHeading}${WEIGHT_QUERY}`;
+    const paragraphLink = `${GOOGLE_FONTS_URL}${newParagraph}${WEIGHT_QUERY}`;
     const link = document.createElement('link');
+
+    const links = document.getElementsByTagName('link');
+
+    let headingLinkExists = false;
+    let paragraphLinkExists = false;
+    for (let item of Array.from(links)) {
+      if (item.href === headingLink) headingLinkExists = true;
+    }
     link.rel = 'stylesheet';
 
-    link.href = headingLink;
-    document.head.appendChild(link);
+    if (!headingLinkExists) {
+      link.href = headingLink;
+      document.head.appendChild(link);
+    }
 
-    link.href = paragraphLink;
-    document.head.appendChild(link);
+    if (!paragraphLinkExists) {
+      link.href = paragraphLink;
+      document.head.appendChild(link);
+    }
   }, [headingFontName, paragraphFontName]);
 
   useEffect(() => {
@@ -60,84 +79,82 @@ const FontSection = () => {
   };
 
   return (
-    <>
-      <Row>
-        <Grid.Container direction="column" gap={2}>
-          <Grid>
-            <Row align="flex-end">
-              <Input
-                label="Heading Font"
-                placeholder="Open Sans"
-                bordered
-                value={heading}
-                onChange={(e) => setHeading(e.target.value)}
-                helperText="Input a font from google fonts"
-              />
-              <Spacer />
-              <Input
-                label="Heading Font"
-                placeholder="Open Sans"
-                bordered
-                value={paragraph}
-                onChange={(e) => setParagraph(e.target.value)}
-                helperText="Input a font from google fonts"
-              />
-              <Spacer />
-              <Button onClick={handleFonts} css={{ minWidth: 'auto' }}>
-                Submit fonts
-              </Button>
-            </Row>
-          </Grid>
-          <Grid>
-            <Spacer y={2} />
-            <Text>Headings</Text>
-            <FontContainer headingFontName={headingFontName} fontWeight="700">
-              <Text>AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz</Text>
-              <Spacer />
-              <Text>1234567890!@£$%^&*()</Text>
-            </FontContainer>
-          </Grid>
-          <Grid>
-            <Text>Paragraphs</Text>
-            <FontContainer headingFontName={paragraphFontName} fontWeight="400">
-              <Text>AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz</Text>
-              <Spacer />
-              <Text>1234567890!@£$%^&*()</Text>
-            </FontContainer>
-          </Grid>
-        </Grid.Container>
-        <Grid.Container direction="column" gap={2}>
-          <Grid>
-            <Row>
-              <Input
-                label="Base size"
-                placeholder="14"
-                bordered
-                type="number"
-                value={`${baseSize}`}
-                onChange={handleBaseSize}
-              />
-              <Spacer />
-              <Dropdown
-                label="Scale Factor"
-                placeholder="Select Factor"
-                options={SCALE_OPTIONS}
-                value={scaleFactor}
-                onChange={(e) => setScaleFactor(e.target.value)}
-              />
-            </Row>
-          </Grid>
-          <Grid>
-            <Spacer y={2} />
-            <Table
-              tableHeaders={TABLE_HEADERS}
-              tableRows={TABLE_ROWS}
-              lastItemStyles={{ p: '0' }}
+    <Row wrap="wrap" justify="space-between">
+      <Grid.Container direction="column" gap={2} css={{ maxWidth: 'fit-content' }}>
+        <Grid>
+          <Row align="flex-end">
+            <Input
+              label="Heading Font"
+              placeholder="Open Sans"
+              bordered
+              value={heading}
+              onChange={(e) => setHeading(e.target.value)}
+              helperText="Input a font from google fonts"
             />
-          </Grid>
-        </Grid.Container>
-      </Row>
-    </>
+            <Spacer />
+            <Input
+              label="Paragraph Font"
+              placeholder="Open Sans"
+              bordered
+              value={paragraph}
+              onChange={(e) => setParagraph(e.target.value)}
+              helperText="Input a font from google fonts"
+            />
+            <Spacer />
+            <Button onClick={handleFonts} css={{ minWidth: 'auto' }}>
+              Submit fonts
+            </Button>
+          </Row>
+        </Grid>
+        <Grid>
+          <Spacer y={2} />
+          <Text>Headings</Text>
+          <FontContainer headingFontName={headingFontName} fontWeight="700">
+            <Text>AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz</Text>
+            <Spacer />
+            <Text>1234567890!@£$%^&*()</Text>
+          </FontContainer>
+        </Grid>
+        <Grid>
+          <Text>Paragraphs</Text>
+          <FontContainer headingFontName={paragraphFontName} fontWeight="400">
+            <Text>AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz</Text>
+            <Spacer />
+            <Text>1234567890!@£$%^&*()</Text>
+          </FontContainer>
+        </Grid>
+      </Grid.Container>
+      <Grid.Container direction="column" gap={2} css={{ maxWidth: '650px' }}>
+        <Grid>
+          <Row>
+            <Input
+              label="Base size"
+              placeholder="14"
+              bordered
+              type="number"
+              value={`${baseSize}`}
+              onChange={handleBaseSize}
+            />
+            <Spacer />
+            <Dropdown
+              label="Scale Factor"
+              placeholder="Select Factor"
+              options={SCALE_OPTIONS}
+              value={scaleFactor}
+              onChange={(e) => setScaleFactor(e.target.value)}
+            />
+          </Row>
+        </Grid>
+        <Grid>
+          <Spacer y={2} />
+          <Table
+            tableHeaders={TABLE_HEADERS}
+            tableRows={TABLE_ROWS}
+            lastItemStyles={{ p: '0' }}
+          />
+        </Grid>
+      </Grid.Container>
+    </Row>
   );
 };
 
