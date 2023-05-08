72618c694da86fb04001decfb4d452c36d2e4ee2
Alexandra Oreza <A18016321@alumnos.uady.mx>
2023-05-07 23:45:46 -0600

    final test update

--- cypress/e2e/MR06/MR06-EM01.cy.ts
+++ cypress/e2e/MR06/MR06-EM01.cy.ts
@@ -16,58 +16,54 @@ describe('MR06-EM01 Testing', () => {
 
     it('Adds to a color that passes the contrast test', () => {
         //Adds #ffffff to TextColor to contrast agains bgColor #000000
-        cy.get('div[title=grid-TestColor] > div[title=addColor]').click()
+        cy.get('div[title=grid-TextColor] > div[title=addColor]').click()
         cy.get('input[name=colorInput]').type('{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}ffffff')
         cy.get('button[name=saveBtn]').click()
-        cy.get('div[title=errorMessage]').should('not.be.visible')
       })
 
       it('Adds to a color that barely passes the contrast test', () => {
         //Adds #C85F5F to TextColor to contrast agains bgColor #000000
-        cy.get('div[title=grid-TestColor] > div[title=addColor]').click()
+        cy.get('div[title=grid-TextColor] > div[title=addColor]').click()
         cy.get('input[name=colorInput]').type('{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}C85F5F')
         cy.get('button[name=saveBtn]').click()
-        cy.get('div[title=errorMessage]').should('not.be.visible')
       })
 
       it('Adds to a color does not pass the contrast test', () => {
         //Adds #7F1F1F to TextColor to contrast agains bgColor #000000
-        cy.get('div[title=grid-TestColor] > div[title=addColor]').click()
+        cy.get('div[title=grid-TextColor] > div[title=addColor]').click()
         cy.get('input[name=colorInput]').type('{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}7F1F1F')
         cy.get('button[name=saveBtn]').click()
-        cy.get('div[title=errorMessage]').should('be.visible')
+        cy.get('small[title=errorMsg]').should('be.visible')
       })
 
       it('Updates to a color that passes the contrast test', () => {
         //Adds #ffffff to TextColor and then changes it to #e49999 to contrast agains bgColor #000000
-        cy.get('div[title=grid-TestColor] > div[title=addColor]').click()
+        cy.get('div[title=grid-TextColor] > div[title=addColor]').click()
         cy.get('input[name=colorInput]').type('{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}ffffff')
         cy.get('button[name=saveBtn]').click()
-        cy.get('div[title=grid-TestColor] > div[title=addColor]').click()
+        cy.get('div[title=grid-TextColor] > div[title=card0]').click()
         cy.get('input[name=colorInput]').type('{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}e49999')
         cy.get('button[name=saveBtn]').click()
-        cy.get('div[title=errorMessage]').should('not.be.visible')
       })
 
       it('Updates to a color that barely passes the contrast test', () => {
         //Adds #ffffff to TextColor and then changes it to #C85F5F to contrast agains bgColor #000000
-        cy.get('div[title=grid-TestColor] > div[title=addColor]').click()
+        cy.get('div[title=grid-TextColor] > div[title=addColor]').click()
         cy.get('input[name=colorInput]').type('{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}ffffff')
         cy.get('button[name=saveBtn]').click()
-        cy.get('div[title=grid-TestColor] > div[title=addColor]').click()
+        cy.get('div[title=grid-TextColor] > div[title=card0]').click()
         cy.get('input[name=colorInput]').type('{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}C85F5F')
         cy.get('button[name=saveBtn]').click()
-        cy.get('div[title=errorMessage]').should('not.be.visible')
       })
 
       it('Updates to a color does not pass the contrast test', () => {
         //Adds #ffffff to TextColor and then changes it #7F1F1F to TextColor to contrast agains bgColor #000000
-        cy.get('div[title=grid-TestColor] > div[title=addColor]').click()
+        cy.get('div[title=grid-TextColor] > div[title=addColor]').click()
         cy.get('input[name=colorInput]').type('{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}ffffff')
         cy.get('button[name=saveBtn]').click()
-        cy.get('div[title=grid-TestColor] > div[title=addColor]').click()
+        cy.get('div[title=grid-TextColor] > div[title=card0]').click()
         cy.get('input[name=colorInput]').type('{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}7F1F1F')
         cy.get('button[name=saveBtn]').click()
-        cy.get('div[title=errorMessage]').should('be.visible')
+        cy.get('small[title=errorMsg]').should('be.visible')
       })
   })
